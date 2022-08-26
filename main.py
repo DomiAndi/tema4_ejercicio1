5e019859957a6543747521c69350986dae6b2a51
DomiAndi <lnj20120798@gmail.com>
2022-08-26 18:00:04 -0500

    Them.

---
+++ main.py
@@ -0,0 +1,9 @@
+#Escribe un programa que pregunte al usuario su edad y muestre
+# por pantalla si es mayor de edad o no.
+
+edad = int(input("Cual es tu edad: "))
+
+if edad >= 18:
+    print("Es Mayor de Edad")
+else:
+    print("Es Menor de Edad")
